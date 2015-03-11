2e30e2d5a072e26b2a826382754a43df2b2b3c4c
Amanda Kom <mandareneekom@gmail.com>
2015-03-10 17:43:54 -0700

    Unhappy edit tests and code added

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -17,6 +17,20 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+
+    if @project.update_attributes(project_params)
+      redirect_to @project, notice: 'Project was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
   def show
     @project = Project.find(params[:id])
   end
