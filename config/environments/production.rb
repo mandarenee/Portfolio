3cfb7ae21fa4c0d9c13bd1883530a7a1499409e0
Amanda <mandakom@uw.edu>
2017-06-05 18:24:28 -0700

    Updating Mandrill port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   }
   config.action_mailer.smtp_settings = {
     address: "smtp.mandrillapp.com",
-    port: 25, # ports 587 and 2525 are also supported with STARTTLS
+    port: 587, # ports 587 and 2525 are also supported with STARTTLS
     enable_starttls_auto: true, # detects and uses STARTTLS
     user_name: ENV["MANDRILL_USERNAME"],
     password: ENV["MANDRILL_API_KEY"], # SMTP password is any valid API key
